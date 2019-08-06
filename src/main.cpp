f490638d421b12cf4a48c923583fd43303c9b465
Andy Roxby <aroxby@users.noreply.github.com>
2019-08-05 22:34:45 -0400

    Dump some icon stream data

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include <windows.h>
 using namespace std;
 
@@ -15,6 +16,47 @@ extern "C" WINAPI LSTATUS RegGetValueA(
   LPDWORD pcbData
 );
 
+/* Source: https://tmintner.wordpress.com/2011/07/08/
+    windows-7-notification-area-automation-falling-back-down-the-binary-registry-rabbit-hole/*/
+struct IconStreamBlockHeader {
+    union {
+        char unk[20];
+        struct {    // Reverse engineered
+            DWORD unk0;
+            DWORD unk1;
+            WORD unk2;
+            WORD unk3;
+            DWORD streamCount;
+            DWORD unk4;
+        };
+    };
+};
+
+struct IconStream {
+    wchar_t path[528 / 2];
+    DWORD visibility;
+    wchar_t lastTip[512 / 2];
+    char unk0[592];  // Possible NID variant?
+    DWORD unk1;  //App ID?
+};
+
+void rot13(wchar_t *str) {
+    while(*str) {
+        if(*str >= 'A' && *str <= 'Z') {
+            *str -= 'A';
+            *str += 13;
+            *str %= 26;
+            *str += 'A';
+        }
+        else if(*str >= 'a' && *str <= 'z') {
+            *str -= 'a';
+            *str += 13;
+            *str %= 26;
+            *str += 'a';
+        }
+        str++;
+    }
+}
 
 int main() {
     const char *keyPath = "Software\\Classes\\Local Settings"
@@ -28,19 +70,39 @@ int main() {
         return 1;
     }
 
-    // TODO: Call with zero length buffer to get the right size
-    const static int bufLen = 1024 * 1024;
-    unsigned char buffer[bufLen];
-    DWORD mBufLen = bufLen;
-    status = RegGetValueA(hKey, nullptr, "IconStreams", RRF_RT_REG_BINARY, nullptr, buffer, &mBufLen);
+    DWORD bufLen;
+    status = RegGetValueA(hKey, nullptr, "IconStreams", RRF_RT_REG_BINARY, nullptr, nullptr, &bufLen);
+    if(status) {
+        cerr << "Failed to read size with error: " << status << endl;
+        return 2;
+    }
+
+    assert((bufLen - sizeof(IconStreamBlockHeader)) % sizeof(IconStream) == 0);
+
+    unsigned char *buffer = new unsigned char[bufLen];
+    status = RegGetValueA(hKey, nullptr, "IconStreams", RRF_RT_REG_BINARY, nullptr, buffer, &bufLen);
 
     if(status) {
         cerr << "Failed to read data with error: " << status << endl;
         return 3;
     }
 
-    for(int i =0; i<10; i++) {
-        printf("%02x ", (unsigned int)(buffer[i]));
+    IconStreamBlockHeader *header = (IconStreamBlockHeader *)buffer;
+    buffer += sizeof(IconStreamBlockHeader);
+    bufLen -= sizeof(IconStreamBlockHeader);
+    int numStreams = bufLen / sizeof(IconStream);
+
+    printf("Header: ");
+    for(int i = 0; i < sizeof(IconStreamBlockHeader); i++) {
+        printf("%02x ",(int)(header->unk[i]));
+    }
+    printf("\n");
+
+    for(int i = 0; i < numStreams; i++) {
+        IconStream *stream = (IconStream*)(buffer);
+        stream += i;
+        rot13(stream->path);
+        printf("%S\n", stream->path);
     }
     printf("\n");
 
